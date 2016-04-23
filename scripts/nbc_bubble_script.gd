2d595d3cc0f189bf0315ab10bc204a1d8f5e33d4
MartiniMoe <martini.moe@cheex.de>
2016-04-23 14:03:18 +0300

    Make dialog closable

---
+++ scripts/nbc_bubble_script.gd
@@ -0,0 +1,15 @@
+
+extends VBoxContainer
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+
+func _on_Button_pressed():
+	hide()
