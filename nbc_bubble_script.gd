4cda2bf379b1a70620586c7b69123d97dc8c9343
MartiniMoe <martini.moe@cheex.de>
2016-04-26 12:48:17 +0300

    change npc dialog

--- nbc_bubble_script.gd
+++ nbc_bubble_script.gd
@@ -1,5 +1,5 @@
 
-extends VBoxContainer
+extends Panel
 
 # member variables here, example:
 # var a=2
@@ -9,3 +9,7 @@ func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
+
+func _on_text_interface_engine_input_event( ev ):
+	if ev.type == 3:
+		hide()
--- scripts/dummy_scene.gd
+++ scripts/dummy_scene.gd
@@ -4,6 +4,8 @@ extends Navigation2D
 const SPEED = 300.0
 const TALK_DISTANCE = 50
 
+var testNpcClicked = false
+
 var begin = Vector2()
 var end = Vector2()
 var path = []
@@ -29,23 +31,43 @@ func _process(delta):
 		if (path.size() < 2):
 			path = []
 			set_process(false)
+			print("path complete")
+			if checkTestNpcNear() && testNpcClicked:
+				get_node("npc_test/npc_bubble/text_interface_engine").reset()
+				get_node("npc_test/npc_bubble").show()
+				get_node("npc_test/npc_bubble/text_interface_engine").buff_text("Salam aleikum!\n", 0.1)
+				get_node("npc_test/npc_bubble/text_interface_engine").buff_silence(0.1)
+				get_node("npc_test/npc_bubble/text_interface_engine").buff_text("5uie", 0.1)
+				get_node("npc_test/npc_bubble/text_interface_engine").set_state(1)
 	else:
 		set_process(false)
 
 
-func checkNpcClickedAndNear(clickPos):
+func checkTestNpcClicked(clickPos):
 	var character = get_node("character")
-	var npc = get_node("npc")
+	var npc = get_node("npc_test")
 	var npc_x = npc.get_pos().x
 	var npc_y = npc.get_pos().y
 	var npc_width = npc.get_region_rect().size.x
 	var npc_height = npc.get_region_rect().size.y
 	if (clickPos.x > (npc_x - npc_width/2) && clickPos.x < (npc_x + npc_width/2)):
 		if (clickPos.y > (npc_y - npc_height/2) && clickPos.y < (npc_y + npc_height/2)):
-			# check if character is near
-			if character.get_pos().x > (npc_x - npc_width/2 - TALK_DISTANCE) && character.get_pos().x < (npc_x + npc_width/2 + TALK_DISTANCE):
-				if character.get_pos().y > (npc_y - npc_height/2 - TALK_DISTANCE) && character.get_pos().y < (npc_y + npc_height/2 + TALK_DISTANCE):
-					return true
+			return true
+	return false
+
+
+func checkTestNpcNear():
+	var character = get_node("character")
+	var npc = get_node("npc_test")
+	var npc_x = npc.get_pos().x
+	var npc_y = npc.get_pos().y
+	var npc_width = npc.get_region_rect().size.x
+	var npc_height = npc.get_region_rect().size.y
+	if character.get_pos().x > (npc_x - npc_width/2 - TALK_DISTANCE) && character.get_pos().x < (npc_x + npc_width/2 + TALK_DISTANCE):
+		if character.get_pos().y > (npc_y - npc_height/2 - TALK_DISTANCE) && character.get_pos().y < (npc_y + npc_height/2 + TALK_DISTANCE):
+			return true
+	return false
+
 
 func _update_path():
 	var p = get_simple_path(begin, end, true)
@@ -59,8 +81,12 @@ func _input(event):
 	if (event.type == InputEvent.MOUSE_BUTTON and event.pressed and event.button_index == 1):
 		begin = get_node("character").get_pos()
 		
-		if checkNpcClickedAndNear(event.pos - get_pos()):
-			get_node("npc/npc_bubble").show()
+		if checkTestNpcClicked(event.pos - get_pos()):
+			testNpcClicked = true
+			print("npc clicked")
+		else:
+			testNpcClicked = false
+			print("npc not clicked")
 		
 		# Mouse to local navigation coordinates
 		end = event.pos - get_pos()
@@ -68,6 +94,6 @@ func _input(event):
 
 
 func _ready():
-	get_node("npc/npc_bubble").hide()
+	get_node("npc_test/npc_bubble").hide()
 	set_process_input(true)
 
