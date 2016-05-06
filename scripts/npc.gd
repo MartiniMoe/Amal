97d11388ad613a6de4a1a8923e037eefa555f458
Jasmin <jasmin.kirchhuebel@gmx.net>
2016-05-06 13:33:05 +0300

    conversation with old woman in entrance_side
    npcs will only get triggered when they are set visible

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -7,22 +7,26 @@ func _ready():
 	pass
 
 func show_dialogue():
-	get_parent().dialogue_running = true
-	get_node("../npc_bubble/text_interface_engine").reset()
-	get_node("../npc_bubble").show()
-	while (counter < npc_text.size()):
-		if(npc_text[counter] == "stop"):
+	if(!self.is_visible()):
+		return
+	else:
+		counter = 0
+		get_parent().dialogue_running = true
+		get_node("../npc_bubble/text_interface_engine").reset()
+		get_node("../npc_bubble").show()
+		while (counter < npc_text.size()):
+			if(npc_text[counter] == "stop"):
+				counter = counter + 1
+				break
+			else:
+				#get_node("../npc_bubble/text_interface_engine").buff_text(npc_text[counter] + "\n", 0.1)
+				get_node("../npc_bubble/text_interface_engine").buff_text(npc_text[counter].right(npc_text[counter].find_last("_")+1) + ": ")
+				get_node("../npc_bubble/text_interface_engine").buff_text(tr(npc_text[counter]) + "\n", 0.1)
+				get_node("../npc_bubble/text_interface_engine").buff_silence(0.1)
+				counter = counter + 1
+				break
 			counter = counter + 1
-			break
-		else:
-			#get_node("../npc_bubble/text_interface_engine").buff_text(npc_text[counter] + "\n", 0.1)
-			get_node("../npc_bubble/text_interface_engine").buff_text(npc_text[counter].right(npc_text[counter].find_last("_")+1) + ": ")
-			get_node("../npc_bubble/text_interface_engine").buff_text(tr(npc_text[counter]) + "\n", 0.1)
-			get_node("../npc_bubble/text_interface_engine").buff_silence(0.1)
-			counter = counter + 1
-			break
-		counter = counter + 1
-	get_node("../npc_bubble/text_interface_engine").set_state(1)
+		get_node("../npc_bubble/text_interface_engine").set_state(1)
 
 func hide_dialogue():
 	get_parent().dialogue_running = false
